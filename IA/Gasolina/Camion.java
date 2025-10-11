c15e9dcd768f2e952edf7aeda5eae33b607a55f1
Kai K <kai@kaiknox.com>
2025-10-11 12:02:19 +0200

    Make all objects extend coordinates

--- IA/Gasolina/Camion.java
+++ IA/Gasolina/Camion.java
@@ -1,20 +1,8 @@
 package IA.Gasolina;
 
-public class Camion {
-    
-    private int CoordX;
-    private int CoordY;
+public class Camion extends Coordenada {
 
     public Camion(int x, int y){
-        this.CoordX = x;
-        this.CoordY = y;
-    }
-
-    public int getCoordX() {
-        return CoordX;
-    }
-
-    public int getCoordY() {
-        return CoordY;
+        super(x, y);
     }
 }
